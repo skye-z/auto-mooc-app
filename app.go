0876ee86c461081fb6f3982e377b5db11acb5784
skay <skai-zhang@hotmail.com>
2024-03-18 14:59:45 +0800

    edit work path

--- app.go
+++ app.go
@@ -3,32 +3,37 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/skye-z/auto-mooc/service"
 	"github.com/skye-z/auto-mooc/util"
 )
 
-// App struct
 type App struct {
 	ctx context.Context
 }
 
-// NewApp creates a new App application struct
 func NewApp() *App {
 	return &App{}
 }
 
-// startup is called when the app starts. The context is saved
-// so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 
 	util.InitConfig()
+	util.Set("basic.workspace", a.UpdateConfigPath())
 	router := service.InitNoPageRouter()
 	service.RunRouter(router)
 }
 
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+func (a *App) UpdateConfigPath() string {
+	// 获取用户配置文件目录
+	configDir, _ := os.UserConfigDir()
+	configDir = fmt.Sprintf("%s/AutoMooc", configDir)
+	// 判断应用目录是否存在
+	if _, err := os.Stat(configDir); os.IsNotExist(err) {
+		// 目录不存在,创建目录
+		os.Mkdir(configDir, os.ModePerm)
+	}
+	return configDir
 }
